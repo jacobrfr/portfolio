61df3394213adcd2790433d0f042b41fa7fef9ea
Jacob Franklin <jacobfranklin217@gmail.com>
2022-10-17 18:21:39 -0500

    add templates and display actual projects

--- src/projects/models.py
+++ src/projects/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Project(models.Model):
+    title = models.TextField()
+    summary = models.TextField()
+    skills = models.TextField()
+    pub_date = models.DateTimeField("date published")
+    github = models.URLField()
+    external = models.URLField()
+    thumbnail = models.ImageField()
+
+    def __str__(self):
+        return self.title
--- src/projects/views.py
+++ src/projects/views.py
@@ -1,4 +1,12 @@
-from django.http import HttpResponse
+from django.http import get_object_or_404, render
+
+from .models import Project
 
 def index(request):
-    return HttpResponse("projects index page")
+    projects = Project.objects.order_by('-pub_date')
+    context = {'projects': projects}
+    return render(request, 'projects/index.html', context)
+
+def detail(request, project_id):
+    project = get_object_or_404(Project, pk=project_id)
+    return render(request, 'projects/detail.html', {'project': project})
