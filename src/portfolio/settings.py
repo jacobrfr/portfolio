4320fa704c9fef1b0a77394122e746c66d7bc2d8
Jacob Franklin <jacobfranklin217@gmail.com>
2022-10-21 13:25:25 -0500

    allowed hosts

--- src/portfolio/settings.py
+++ src/portfolio/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['3.137.176.183']
+ALLOWED_HOSTS = ['18.218.218.13']
 
 CSRF_COOKIE_SECURE = False
 
